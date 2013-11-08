cc93db1f9e005822989eb288e38cf29b68aa0703
Andrew Straw <strawman@astraw.com>
2013-11-08 16:59:21 +0100

    install test sub-packages

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from distutils.core import setup
 setup(name='pymvg',
       version='1.0',
-      packages=['pymvg'],
+      packages=['pymvg',
+                'pymvg.test',
+                'pymvg.test.external.cv',
+                'pymvg.test.external.ros',
+                'pymvg.test.external.mcsc',
+                ],
       )
