528154a9e4d3a2642fc5106a306e7e299bd8b3cf
Andrew Straw <strawman@astraw.com>
2014-08-11 01:29:40 +0200

    testing: add test for camera equality

--- pymvg/test/test_camera_model.py
+++ pymvg/test/test_camera_model.py
@@ -327,6 +327,25 @@ def test_simple_camera():
                                          eye=center,
                                          lookat=lookat)
 
+def test_equality():
+    center = np.array( (0, 0.0, 5) )
+    lookat = center + np.array( (0,1,0))
+    cam_apple1 = CameraModel.load_camera_simple(fov_x_degrees=90,
+                                                eye=center,
+                                                lookat=lookat,
+                                                name='apple')
+    cam_apple2 = CameraModel.load_camera_simple(fov_x_degrees=90,
+                                                eye=center,
+                                                lookat=lookat,
+                                                name='apple')
+    cam_orange = CameraModel.load_camera_simple(fov_x_degrees=30,
+                                                eye=center,
+                                                lookat=lookat,
+                                                name='orange')
+    assert cam_apple1==cam_apple2
+    assert cam_apple1!=cam_orange
+    assert not cam_apple1==cam_orange
+
 def test_pickle_roundtrip():
     all_options = get_default_options()
     for opts in all_options:
