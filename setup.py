2fc74de1f78e17f4c7bbddde8b4b8d2c328cb14e
Andrew Straw <strawman@astraw.com>
2014-12-13 23:54:20 +0100

    enable 'python setup.py test'

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
-from distutils.core import setup
+from distutils.core import setup, Command
+
+class NoseTestCommand(Command):
+    user_options = [ ]
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import nose
+        nose.run_exit(argv=['nosetests'])
+
 setup(name='pymvg',
       version='1.1',
       packages=['pymvg',
@@ -10,4 +24,5 @@ setup(name='pymvg',
       url='https://github.com/strawlab/pymvg',
       package_data={ 'pymvg.test': ['*.yaml','*.json'],
                      },
+      cmdclass={'test': NoseTestCommand},
       )
